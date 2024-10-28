49d95cc398f4fc6efc260882b5d33da1919155ba
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-28 15:57:41 +0330

    add img to CharacterDetail

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,5 +1,14 @@
+import { character } from "../../data/data";
+
 function CharacterDetail() {
-  return <div style={{flex: 1}}></div>;
+  return (
+    <div style={{ flex: 1 }}>
+      <div className="character-detail">
+        <img src={character.image} alt={character.name} />
+      </div>
+      <div className="character-episodes"></div>
+    </div>
+  );
 }
 
 export default CharacterDetail;
