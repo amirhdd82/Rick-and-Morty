b09586ab51f0f64d5518a3ba4405582146f0052a
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-28 16:05:58 +0330

    add info to characterDetail

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -4,7 +4,17 @@ function CharacterDetail() {
   return (
     <div style={{ flex: 1 }}>
       <div className="character-detail">
-        <img src={character.image} alt={character.name} />
+        <img
+          src={character.image}
+          alt={character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+            <h3 className="name">
+                <span>{character.gender === "Male" ? "🧔🏻‍♂️" : "👩🏻‍🦱"}</span>
+                <span>{character.name}</span>
+            </h3>
+        </div>
       </div>
       <div className="character-episodes"></div>
     </div>
