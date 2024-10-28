b8b6869da25242d4bf69955463f3752d2d115337
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-28 12:18:47 +0330

    add span tag in button with badge className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
       <div className="navbar__result">Found X character</div>
       <button className="heart">
         <HeartIcon className="icon"/>
+        <span className="badge">4</span>
       </button>
     </div>
   );
