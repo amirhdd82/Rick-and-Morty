6cdc742b08c03de9bc30d01b82075720d7fc2793
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-28 16:10:07 +0330

    add status to characterDetail

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -10,10 +10,14 @@ function CharacterDetail() {
           className="character-detail__img"
         />
         <div className="character-detail__info">
-            <h3 className="name">
-                <span>{character.gender === "Male" ? "🧔🏻‍♂️" : "👩🏻‍🦱"}</span>
-                <span>{character.name}</span>
-            </h3>
+          <h3 className="name">
+            <span>{character.gender === "Male" ? "🧔🏻‍♂️" : "👩🏻‍🦱"}</span>
+            <span> {character.name}</span>
+          </h3>
+          <div className="info">
+            <span className={`status ${character.status === "Dead" ? "red" : ""}`}></span>
+            
+          </div>
         </div>
       </div>
       <div className="character-episodes"></div>
