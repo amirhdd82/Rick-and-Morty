9984564e166e159a6d3aa3ab876062e587dfcb8d
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-28 12:21:00 +0330

    import CharacterList to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
+import CharacterList from "./components/CharacterList";
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <CharacterList />
     </div>
   );
 }
