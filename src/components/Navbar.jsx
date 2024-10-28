92183f4bb6bc8bbadb73daec32d420bbb54fcb76
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-28 12:17:01 +0330

    use button with heart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {heatIcon} from "@heroicons/react/outline/24"
+import {HeartIcon} from "@heroicons/react/24/outline"
 
 function Navbar() {
   return (
@@ -6,7 +6,9 @@ function Navbar() {
       <div className="navbar__logo">LOGO🧌</div>
       <input type="text" className="text-field" placeholder="search..." />
       <div className="navbar__result">Found X character</div>
-      
+      <button className="heart">
+        <HeartIcon className="icon"/>
+      </button>
     </div>
   );
 }
