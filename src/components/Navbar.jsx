3dbf99bfb2c886361917ae4e2f86458d8eb3cc48
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-29 19:45:34 +0330

    add logo function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
-import {HeartIcon} from "@heroicons/react/24/outline"
+import { HeartIcon } from "@heroicons/react/24/outline";
 
-function Navbar({numOfResult }) {
+function Navbar({ numOfResult }) {
   return (
     <div className="navbar">
-      <div className="navbar__logo">LOGO🧌</div>
+      <Logo />
       <input type="text" className="text-field" placeholder="search..." />
       <div className="navbar__result">Found {numOfResult} character</div>
       <button className="heart">
-        <HeartIcon className="icon"/>
+        <HeartIcon className="icon" />
         <span className="badge">4</span>
       </button>
     </div>
   );
 }
 
-
+function Logo() {
+  return <div className="navbar__logo">LOGO🧌</div>;
+}
 
 export default Navbar;
