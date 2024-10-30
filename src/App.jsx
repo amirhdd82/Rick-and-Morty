82f6243c21a9ad2479f33cbfec3a1b6e0e48217e
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-30 11:46:37 +0330

    fetch character api

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,34 @@ import Navbar, { SearchResult } from "./components/Navbar";
 import "./App.css";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Allcharacters } from "../data/data";
 function App() {
-  const [characters, setCharacters] = useState(Allcharacters );
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacters(data.results.slice(0, 5)));
+  }, []);
 
   return (
     <div className="app">
       <Navbar>
-        <SearchResult numOfResult={characters.length}/>
+        <SearchResult numOfResult={characters.length} />
       </Navbar>
       <Main>
-      <div className="main">
-        <CharacterList characters={characters }/>
-        <CharacterDetail />
-      </div>
+        <div className="main">
+          <CharacterList characters={characters} />
+          <CharacterDetail />
+        </div>
       </Main>
     </div>
   );
 }
 
-function Main({children}){
-  return (
-    <div className="main">
-      {children}
-    </div>
-  )
+function Main({ children }) {
+  return <div className="main">{children}</div>;
 }
 
 export default App;
