e9dd378a4c09c1a7fbd804d22b76d97481bae9e8
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-10-31 09:45:15 +0330

    add try catch for manage error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
 import { useEffect, useState } from "react";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [characters, setCharacters] = useState([]);
@@ -14,10 +14,15 @@ function App() {
       try {
         setIsLoading(true);
         const res = await fetch("https://rickandmortyapi.com/api/character");
+        if(!res.ok) throw new Error("Something went wrong!")
         const data = await res.json();
         setCharacters(data.results.slice(0, 5));
         setIsLoading(false);
-      } catch (error) {}
+      } catch (error) {
+        toast.error(error.message)
+      }finally{
+        setIsLoading(false)
+      }
     }
     fetchData();
   }, []);
