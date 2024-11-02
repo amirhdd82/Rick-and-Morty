a39ff1d5bb74c91039817beb3b602309ee1ce31b
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-11-02 09:59:47 +0330

    add charactersubInf to CharacterDetail compo

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -49,42 +49,6 @@ function CharacterDetail({ selectedId, onAddFavourite, isAddToFavourite }) {
 
   return (
     <div style={{ flex: 1 }}>
-      <div className="character-detail">
-        <img
-          src={character.image}
-          alt={character.name}
-          className="character-detail__img"
-        />
-        <div className="character-detail__info">
-          <h3 className="name">
-            <span>{character.gender === "Male" ? "🧔🏻‍♂️" : "👩🏻‍🦱"}</span>
-            <span> {character.name}</span>
-          </h3>
-          <div className="info">
-            <span
-              className={`status ${character.status === "Dead" ? "red" : ""}`}
-            ></span>
-            <span> {character.status}</span>
-            <span> - {character.species}</span>
-          </div>
-          <div className="location">
-            <p>last known location:</p>
-            <p>{character.location.name}</p>
-          </div>
-          <div className="actions">
-            {isAddToFavourite ? (
-              <p>Already added to Favourites</p>
-            ) : (
-              <button
-                onClick={() => onAddFavourite(character)}
-                className="btn btn--primary"
-              >
-                Add to Favourite
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
       <div className="character-episodes">
         <div className="title">
           <h2>list of episodes</h2>
@@ -103,6 +67,47 @@ function CharacterDetail({ selectedId, onAddFavourite, isAddToFavourite }) {
             </li>
           ))}
         </ul>
+      </div>
+    </div>
+  );
+}
+
+function CharacterSubInfo() {
+  return (
+    <div className="character-detail">
+      <img
+        src={character.image}
+        alt={character.name}
+        className="character-detail__img"
+      />
+      <div className="character-detail__info">
+        <h3 className="name">
+          <span>{character.gender === "Male" ? "🧔🏻‍♂️" : "👩🏻‍🦱"}</span>
+          <span> {character.name}</span>
+        </h3>
+        <div className="info">
+          <span
+            className={`status ${character.status === "Dead" ? "red" : ""}`}
+          ></span>
+          <span> {character.status}</span>
+          <span> - {character.species}</span>
+        </div>
+        <div className="location">
+          <p>last known location:</p>
+          <p>{character.location.name}</p>
+        </div>
+        <div className="actions">
+          {isAddToFavourite ? (
+            <p>Already added to Favourites</p>
+          ) : (
+            <button
+              onClick={() => onAddFavourite(character)}
+              className="btn btn--primary"
+            >
+              Add to Favourite
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
